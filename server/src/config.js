78a9b2994e52bbf54f6b99da3d1b98fb29c39bab
Avdan Aščić <117448754+avdan-ascic@users.noreply.github.com>
2023-09-26 22:04:54 +0200

    Update config.js

--- server/src/config.js
+++ server/src/config.js
@@ -1,10 +1,10 @@
 import "dotenv/config";
 
 const config = {
-  port: process.env.PORT,
-  secret: process.env.JWT_SECRET,
-  mongo: process.env.MONGO,
-  session_secret: process.env.SESSION_SECRET,
+  port: process.env.PORT || 5000,
+  jwt_secret: process.env.JWT_SECRET || "EnterJWTSecretHere!",
+  mongo: process.env.MONGO || "mongodb://localhost:27017/learning-website",
+  session_secret: process.env.SESSION_SECRET || "EnterSessionSecretHere!",
 };
 
 export default config;
