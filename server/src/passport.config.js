8e8ee55c7eba465f13ccfe6040022ed79b5a4871
Avdan Aščić <117448754+avdan-ascic@users.noreply.github.com>
2023-09-26 22:03:19 +0200

    Update passport.config.js

--- server/src/passport.config.js
+++ server/src/passport.config.js
@@ -1,34 +1,32 @@
-import passport from 'passport'
-import { Strategy as JwtStrategy } from 'passport-jwt'
+import { Strategy as JwtStrategy } from "passport-jwt";
+import passport from "passport";
 
-import User from './models/user.model'
-import config from './config'
+import User from "./models/user.model";
+import config from "./config";
 
-var opts = {}
+let opts = {};
 opts.jwtFromRequest = function (req) {
-  var token = null
-  if (req && req.cookies) {
-    token = req.cookies.token;
-  }
+  let token = null;
+  if (req && req.cookies) token = req.cookies.token;
   return token;
-}
-opts.secretOrKey = config.secret
+};
+opts.secretOrKey = config.jwt_secret;
 
-
-passport.use(new JwtStrategy(opts, async function (jwt_payload, done) {
-  User.findById(jwt_payload.id)
-    .then(user => {
-      if (!user) return done(null, false)
-      return done(null, user)
-    })
-    .catch(err => done(err, false))
-}))
+passport.use(
+  new JwtStrategy(opts, async function (jwt_payload, done) {
+    User.findById(jwt_payload.id)
+      .then((user) => {
+        if (!user) return done(null, false);
+        return done(null, user);
+      })
+      .catch((err) => done(err, false));
+  })
+);
 
 passport.serializeUser((user, done) => {
-  done(null, user.id)
-})
+  done(null, user._id);
+});
 
 passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then(user => done(null, user))
-})
+  User.findById(id).then((user) => done(null, user));
+});
